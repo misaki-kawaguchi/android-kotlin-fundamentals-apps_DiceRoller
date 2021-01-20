aee9c0786064eb6829842d5bc985fa5ec6977da6
川口美咲 <misaki@arcphilia.co.jp>
2021-01-20 18:27:06 +0900

    [Add]Buttonをクリックするとランダムでサイコロが出るようにする

--- app/src/main/java/com/misakikawaguchi/diceroller/MainActivity.kt
+++ app/src/main/java/com/misakikawaguchi/diceroller/MainActivity.kt
@@ -2,10 +2,32 @@ package com.misakikawaguchi.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = (1..6).random()
+        val diceImage = findViewById<ImageView>(R.id.dice_image)
+
+        val drawableResource = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
